d7c62d9dde359becb92aed5883026351b7f2f923
andresk-devt <learningxandres@gmail.com>
2023-03-12 14:09:54 -0500

    Despliegue de GitHub Pages

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'production'
+  ? '/andresk-devt.github.io/'
+  : '/',
+  outputDir: 'dist',
 
   pluginOptions: {
     i18n: {
